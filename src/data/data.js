a19ea2a21624c9a802873f3f419e432224b51672
Francisco García <francoffee57@gmail.com>
2022-01-01 21:20:29 -0600

    Configuración inicial del proyecto

---
+++ src/data/data.js
@@ -0,0 +1,146 @@
+// Datos que necesta el proyecto:
+    // 1. Nombre del proyecto.
+    // 2. Imagen o gif del proyecto | en baja y alta calidad  
+    // 3. url al proyecto
+    // 4. url del repositorio
+    // 5. Descripción 
+    // 6. Las tecnologias que utilice para construir ese proyecto.
+
+    const  technologies ={
+        HTML:{
+            name: "HTML",
+            icon: "url-del icono"
+        },
+        CSS:{
+            name: "CSS",
+            icon: "url-del icono"
+        },
+        JavaScript:{
+            name: "JavaScript",
+            icon: "url-del icono"
+        },
+        React:{
+            name: "React",
+            icon: "url-del icono"
+        },
+        webpack:{
+            name: "webpack",
+            icon: '../assets/icon/webpack'
+        }
+    }
+    
+    const proyectos = [
+        todoReactApp={
+            name: "Todo React App",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.CSS, technologies.React],
+        },
+        lazyFoxy={
+            name: "Lazy Foxy",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.CSS, technologies.JavaScript],
+        },
+        cateDeMiCorazon={
+            name: "Cate de mi Corazón",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS,technologies.JavaScript],
+        },
+        profesionalJavascript={
+            name: 'Proyecto del curso profesional de JavaScript',
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS,technologies.JavaScript],
+        },
+        bataBit={
+            name: "Bata Bit",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS],
+        },
+        clonGoogle={
+            name: "Clon de Google",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS],
+        },
+        redesLink={
+            name: "Links de mis redes sociales",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS,technologies.JavaScript,technologies.webpack],
+        },
+        primeraWeb={
+            name: "Mi primera página web",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS],
+        },
+        calcFiguras={
+            name: "Calculadora de figuras geométricas",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS,technologies.JavaScript],
+        },
+        pokeball={
+            name: "Dibujo de Pokeball con CSS",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS],
+        },
+    
+    
+        superCAt={
+            name: "Súper Cat",
+            image: "../assets/image/",
+            url: "https//frangardev.github.io/",
+            repository: "fa",
+            description: "",
+            tech: [technologies.HTML,technologies.CSS],
+        },
+    
+    
+    
+        spaMarvel={
+            name: "SPA Marvel",
+            image: "../assets/image/spa-marvel",
+            url: "https//frangardev.github.io/spa-marvel",
+            repository: "fa",
+            description: "SPA hecha en javscript vanila, en la cual me conecte a la api de marvel para mostrar la información.",
+            tech: [technologies.CSS,technologies.JavaScript,technologies.webpack]
+        },
+        gridGallery={
+            name: "Grid Gallery",
+            image: "../assets/image/grid-gallery",
+            url: "https//frangardev.github.io/grid-gallery",
+            repository: "fa",
+            description: "-------------------------",
+            tech: [technologies.HTML,technologies.CSS]
+        }
+    ]
+    
+    export {proyectos}
+    
+    
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        path: "/"
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        // alias:{
+        //     //Estructura de archivos
+        //     '@components': path.resolve(__dirname, 'src/components/'),
+        //     '@app': path.resolve(__dirname, 'src/app/'),
+        //     // '@data': path.resolve(__dirname, 'src/data/'),
+        // }
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    optimization:{
+        minimizer: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
+}
